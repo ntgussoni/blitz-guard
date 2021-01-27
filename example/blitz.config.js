4c0819e0d4ee93072aa83bef545a2af79804f513
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2021-01-27 23:11:21 +0100

    Adds middleware to example app

--- example/blitz.config.js
+++ example/blitz.config.js
@@ -1,11 +1,16 @@
 const { sessionMiddleware, simpleRolesIsAuthorized } = require("@blitzjs/server")
+const { BlitzGuardMiddleware } = require("@blitz-guard/core/dist/middleware")
 
 module.exports = {
   middleware: [
     sessionMiddleware({
       isAuthorized: simpleRolesIsAuthorized,
     }),
+    BlitzGuardMiddleware({
+      excluded: ["/api/auth/mutations/login", "/api/auth/mutations/logout"],
+    }),
   ],
+
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
