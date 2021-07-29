2e1dfdbef9b3ea3d762e1eb3ce39a5a78024aa97
Nicolas <ntorres.dev@gmail.com>
2021-07-29 19:30:10 +0200

    uses ts-jest setup

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,25 +1,5 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  moduleDirectories: ["node_modules", "src"],
-  moduleFileExtensions: ["tsx", "ts", "js", "json"],
-  coverageReporters: ["json", "lcov", "text", "clover"],
-  // collectCoverage: !!`Boolean(process.env.CI)`,
-  modulePathIgnorePatterns: ["<rootDir>/tmp", "<rootDir>/lib"],
-  testTimeout: 30000,
-  coverageDirectory: ".coverage",
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!*.config.{js,ts}",
-    "!**/templates/**",
-    "!**/node_modules/**",
-  ],
-
-  globals: {
-    "ts-jest": {
-      tsConfig: "test/tsconfig.json",
-      isolatedModules: true,
-    },
-  },
 }
